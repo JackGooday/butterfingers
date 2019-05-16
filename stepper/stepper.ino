7880a8f9a0249dff298add0af4fb7ab9856746bd
jackgooday <goodayjack@gmail.com>
2019-05-16 13:35:34 +1000

    First function, starting work on sin movement

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -5,23 +5,33 @@ const int stepsPerRevolution = 200;
 
 void setup()
 {
+  Serial.begin(9600);
+  
   // Declare pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
 }
+
 void loop()
 {
   // Set motor direction clockwise
   digitalWrite(dirPin, HIGH);
 
-  // Spin motor slowly
-  for(int x = 0; x < stepsPerRevolution*2; x++)
-  {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(2000);
+  unsigned long T = 2000000;   //duration in us
+  int S = 1;
+  int O = 0;
+
+  int number_of_steps = S*stepsPerRevolution;
+  unsigned long time_between_steps = T/number_of_steps;
+
+  Serial.println(number_of_steps);
+  Serial.println(time_between_steps);
+
+  for(int x = 0; x < number_of_steps; x++){
+    step_motor(time_between_steps);
+    Serial.println(x);
   }
+
   delay(1000); // Wait a second
   
   // Set motor direction counterclockwise
@@ -30,10 +40,17 @@ void loop()
   // Spin motor quickly
   for(int x = 0; x < stepsPerRevolution; x++)
   {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
+    step_motor(1000);
   }
   delay(1000); // Wait a second
+}
+
+void step_motor(int del){
+  //Steps the motor once
+  //del is the TOTAL duration for this action
+  //del is in MICROseconds
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(del/2);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(del/2);
 }
