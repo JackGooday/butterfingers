47e0104ced31d1bb4cc038282fef02e349a4186c
jackgooday <goodayjack@gmail.com>
2019-05-16 14:32:29 +1000

    Sinusoidal movement

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -12,45 +12,79 @@ void setup()
   pinMode(dirPin, OUTPUT);
 }
 
-void loop()
-{
+void loop(){
+  sin_move();
+}
+
+
+
+void sin_move(){
   // Set motor direction clockwise
   digitalWrite(dirPin, HIGH);
 
-  unsigned long T = 2000000;   //duration in us
-  int S = 1;
+  unsigned long T = 3000;   //duration in ms
+  float S = 2.5;
   int O = 0;
 
+  sin_move(T, S, O);
+}
+
+void sin_move(int T, float S, int O){
+  /*
+   * Moves the stepper in a sinusoidal motion
+   * S = number of revolutions to spin (then spin back)
+   * T = duration for spin forwards then back to take (ms)
+   * O = current offset of motor
+   * 
+   * Requires global variable stepsPerRevolution
+   */
+   
   int number_of_steps = S*stepsPerRevolution;
-  unsigned long time_between_steps = T/number_of_steps;
+  float phi = PI + O;
+  float omega = 2*PI/T;
 
-  Serial.println(number_of_steps);
-  Serial.println(time_between_steps);
-
-  for(int x = 0; x < number_of_steps; x++){
-    step_motor(time_between_steps);
-    Serial.println(x);
-  }
-
-  delay(1000); // Wait a second
+  unsigned long ms = 0;
+  int curr_pos = 0;
+  int next_pos = 0;
+  int no_steps_to_take = 0;
+  int time_between_steps_us = 0;
   
-  // Set motor direction counterclockwise
-  digitalWrite(dirPin, LOW);
+  while(ms<T){
+    //Calculate how many steps we need to take
+    //during this millisecond
+    next_pos = 0.5*number_of_steps*(cos(omega*ms + phi)+1);
+    no_steps_to_take = next_pos - curr_pos;
 
-  // Spin motor quickly
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
-    step_motor(1000);
+    //Print serial for plotting
+    Serial.println(curr_pos);
+
+    //If we need to step backwards, flip the direction
+    if(no_steps_to_take<0){
+      digitalWrite(dirPin, LOW);
+      no_steps_to_take = -no_steps_to_take;
+    }
+    else{
+      digitalWrite(dirPin, HIGH);
+    }
+
+    //Take the steps
+    time_between_steps_us = 1000/no_steps_to_take;  // 1000us / number of steps to take
+    for(int i=0; i<no_steps_to_take; i++){
+      step_motor(time_between_steps_us);
+    }
+    
+    curr_pos = next_pos;
+    ms++;
   }
-  delay(1000); // Wait a second
 }
 
 void step_motor(int del){
   //Steps the motor once
   //del is the TOTAL duration for this action
   //del is in MICROseconds
+  del /= 2;
   digitalWrite(stepPin, HIGH);
-  delayMicroseconds(del/2);
+  delayMicroseconds(del);
   digitalWrite(stepPin, LOW);
-  delayMicroseconds(del/2);
+  delayMicroseconds(del);
 }
