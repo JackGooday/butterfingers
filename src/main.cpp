02c39e53277cb945272e37ce917fbe218f625c4b
telpa <goodayjack@gmail.com>
2019-06-07 13:03:15 +1000

    Position control with pot. Auto power-off

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,16 @@ AccelStepper stepper1(1, 3, 2);
 AccelStepper stepper2(1, 6, 5);
 AccelStepper stepper3(1, 9, 8);
 
+int analogPin = A0;
+int analogVal = 0;
+
 void setup(){
 
   // Set max speeds (steps per second) and accelerations
   stepper1.setMaxSpeed(4000);
   stepper2.setMaxSpeed(1000);
   stepper3.setMaxSpeed(1000);
-  stepper1.setAcceleration(100);
+  stepper1.setAcceleration(200);
   stepper2.setAcceleration(50);
   stepper3.setAcceleration(50);
 
@@ -33,18 +36,29 @@ void setup(){
   pinMode(7, OUTPUT);
   pinMode(10, OUTPUT);
   digitalWrite(4, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(10, HIGH);
+  //digitalWrite(7, HIGH);
+  //digitalWrite(10, HIGH);
 
 }
 
 void loop(){
-  // Change direction at the limits for stepper 1
-  if (stepper1.distanceToGo() == 0){
-      stepper1.moveTo(-stepper1.currentPosition());
+
+  // Read the pot an round to the nearest 2 to stop jitter
+  analogVal = analogRead(analogPin);
+  analogVal /= 2;
+  analogVal *= 2;
+  stepper1.moveTo(analogVal);
+
+  // If the motor is already at the correct position,
+  // switch it off
+  if(stepper1.distanceToGo() == 0){
+    digitalWrite(4, LOW);
+  } else{
+    digitalWrite(4, HIGH);
   }
 
   stepper1.run();
   stepper2.run();
   stepper3.run();
+
 }
